059f3c54feaa0f02c3f247d38973a899208e02aa
Jeremy Mechen <jeremymechen1997@gmail.com>
2019-07-19 23:51:40 +1200

    Paddle gets drawn

--- game/src/game/objects/Paddle.java
+++ game/src/game/objects/Paddle.java
@@ -6,8 +6,21 @@ import processing.core.PApplet;
 public class Paddle implements Drawable {
     public float max_speed = 100;
 
+    private float x;
+    private float y;
+
+    private float width;
+    private float height;
+
+    public Paddle(float x, float y, float width, float height){
+        this.x = x;
+        this.y= y;
+        this.width = width;
+        this.height = height;
+    }
+
     @Override
     public void draw(PApplet d) {
-        // todo
+        d.rect(x, y, width, height);
     }
 }

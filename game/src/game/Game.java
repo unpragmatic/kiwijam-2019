e5241493bb298300fa36b4cd4d84587b7da44b5a
Callum Li <CallumL1009@gmail.com>
2019-07-19 23:11:46 +1200

    Add boilerplate for main game loop

--- game/src/game/Game.java
+++ game/src/game/Game.java
@@ -1,6 +1,31 @@
 package game;
 
+import game.objects.Ball;
+import game.objects.Paddle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game implements GetDrawPayload {
+
+    // todo: Add initialisation params such as position
+    private final Paddle paddle_0 = new Paddle();
+    private final Paddle paddle_1 = new Paddle();
+
+    private final List<Ball> balls = new ArrayList<>();
+
+    public void tick(float delta) {
+        handleCollision();
+        handleInput();
+    }
+
+    public void handleCollision() {
+        // todo.
+    }
+
+    public void handleInput() {
+        // todo.
+    }
 
     @Override
     public DrawPayload getDrawPayload() {

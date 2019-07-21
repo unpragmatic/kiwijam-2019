128cbed799014b56005c295cbfe7dbe27185ff1c
Jeremy Mechen <jeremymechen1997@gmail.com>
2019-07-21 14:42:00 +1200

    Balls have trails

--- game/src/game/objects/Ball.java
+++ game/src/game/objects/Ball.java
@@ -5,6 +5,9 @@ import processing.Drawable;
 import processing.ResourceLoader;
 import processing.core.PApplet;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Ball implements Drawable {
     
     public float x;
@@ -13,10 +16,12 @@ public class Ball implements Drawable {
     public float dy;
     public float radius = 12.5f;
 
+    public ArrayList<int[]> history = new ArrayList<>();
+
     public Ball(float x, float y){
         this.x = x;
         this.y = y;
-        System.out.println("Ball created with world X pos: " + x + " and world y pos: " + y);
+        // System.out.println("Ball created with world X pos: " + x + " and world y pos: " + y);
     }
 
     @Override
@@ -27,6 +32,17 @@ public class Ball implements Drawable {
         int local_size = c.world_to_pixel_width(radius*2, d.width);
 
         d.fill(240, 240, 240);
+
+        if (history.size() > 40) history.remove(0);
+        history.add(new int[]{local_x, local_y});
+
         d.circle(local_x, local_y, local_size);
+
+        for (int i = 0; i < history.size(); i++){
+            d.fill(d.color(240, 240, 240), (i / (float)history.size()) * 255);
+            float size = (i / (float)history.size()) * local_size;
+            d.circle(history.get(i)[0], history.get(i)[1], size);
+        }
+
     }
 }
